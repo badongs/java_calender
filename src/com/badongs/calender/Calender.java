7d44b41727c0a7a3adcfbf5f7e60a0a685bf64ae
badongsch <badongsch@gmail.com>
2018-01-23 12:11:07 +0900

    계산기 입력.

--- src/com/badongs/calender/Calender.java
+++ src/com/badongs/calender/Calender.java
@@ -29,17 +29,33 @@ public class Calender {
 		Scanner scanner = new Scanner(System.in);
 		Calender cal = new Calender();
 
+		String prompt = "cal > ";
 		System.out.println("반목횟수 입력하세요");
-		int repeat = scanner.nextInt();
-		
-		for(int i=0; i<repeat;i++) {
+
+		// int repeat = scanner.nextInt();
+		int month = 1;
+		while (true) {
 			System.out.println("달을 입력하세요");
-			int month = scanner.nextInt();
-			System.out.printf("%d 월은 %d일까지 있습니다.\n", month, cal.getmaxDaysOfMonth(month));
+			System.out.print(prompt); 
+			month = scanner.nextInt();
+			if(month == -1) {
+				break;
+			}
+			
+			if(month > 12) {
+				continue;
+			}
+			System.out.printf("%d 월은 %d일까지 있습니다.\n", month,cal.getmaxDaysOfMonth(month)); 
 		}
-		
+		/*
+		 * for(int i=0; i<repeat;i++) { System.out.println("달을 입력하세요");
+		 * System.out.print(prompt); int month = scanner.nextInt();
+		 * System.out.printf("%d 월은 %d일까지 있습니다.\n", month,
+		 * cal.getmaxDaysOfMonth(month)); }
+		 */
+
 		System.out.println("Bye~~");
-		//cal.printSampleCalender();
+		// cal.printSampleCalender();
 		scanner.close();
 	}
 
