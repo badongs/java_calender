6e1faa5610edf97de2bb5c64c8ac570109d6339a
badongsch <badongsch@gmail.com>
2018-01-23 11:35:27 +0900

    두수의 합 을 구하시요.

---
+++ src/com/badongs/calender/Sum.java
@@ -0,0 +1,27 @@
+package com.badongs.calender;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		
+		Scanner scanner = new Scanner(System.in);
+		String s1, s2;
+		System.out.println("두 수를 입력해 주세요.");
+		s1 = scanner.next();
+		s2 = scanner.next();
+		
+		i = Integer.parseInt(s1);
+		j = Integer.parseInt(s2);
+		
+		//int sum =  i+j;
+		//System.out.println("두 수의 합은 =" + sum);
+		
+		System.out.printf("%d와 %d의 합는 %d 입니다.", i, j ,i+j);
+		
+	}
+
+}
