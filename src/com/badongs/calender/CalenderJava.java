c7be63f28b7f689c5137f1b23368c8ae0a629b60
badongsch <badongsch@gmail.com>
2018-01-23 11:24:15 +0900

    print calender

---
+++ src/com/badongs/calender/CalenderJava.java
@@ -0,0 +1,18 @@
+package com.badongs.calender;
+
+public class CalenderJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Calender");
+		System.out.println(" 일   월   화   수   목   금   토");
+		System.out.println("---------------------");
+		System.out.println(" 1  2  3  4  5  6  7 ");
+		System.out.println(" 8  9 10 11 12 13 14 ");
+		System.out.println("15  16  17  18 19 20 ");
+		System.out.println("21  22 23  24  25 26 ");
+		System.out.println("27  28 29  30  31 ");
+		
+	}
+
+}
