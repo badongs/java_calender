f0e26021f6507b2a66cc8e8da482be7583523a7d
badongsch <badongsch@gmail.com>
2018-01-23 12:02:36 +0900

    달력 월 입력. 반복 수 입력.

--- src/com/badongs/calender/Calender.java
+++ src/com/badongs/calender/Calender.java
@@ -1,10 +1,46 @@
 package com.badongs.calender;
 
+import java.util.Scanner;
+import java.util.Calendar;
+
 public class Calender {
 
-	public static void main(String[] args) {
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 };
+
+	public int getmaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
 		// TODO Auto-generated method stub
+
+	}
+
+	public void printSampleCalender() {
 		System.out.println("Hello, Calender");
+		System.out.println(" 일   월   화   수   목   금   토");
+		System.out.println("---------------------");
+		System.out.println(" 1  2  3  4  5  6  7 ");
+		System.out.println(" 8  9 10 11 12 13 14 ");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+		System.out.println("29 30 31 ");
+
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Calender cal = new Calender();
+
+		System.out.println("반목횟수 입력하세요");
+		int repeat = scanner.nextInt();
+		
+		for(int i=0; i<repeat;i++) {
+			System.out.println("달을 입력하세요");
+			int month = scanner.nextInt();
+			System.out.printf("%d 월은 %d일까지 있습니다.\n", month, cal.getmaxDaysOfMonth(month));
+		}
+		
+		System.out.println("Bye~~");
+		//cal.printSampleCalender();
+		scanner.close();
 	}
 
 }
